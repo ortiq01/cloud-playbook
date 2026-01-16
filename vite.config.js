268a74da2e13aa0d2293062e038225e1107ec019
ortiq01 <ortiq01@gmail.com>
2026-01-16 16:44:17 +0100

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
+  const base = repoName ? `/${repoName}/` : '/'
+
+  return {
+    base,
+    plugins: [react()],
+  }
 })
